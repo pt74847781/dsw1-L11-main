035d76ebadfad19893986686550c176b3d544209
pt74847781 <ariel_sc_2001@hotmail.com>
2023-05-29 11:36:19 -0500

    proyecto inicial

---
+++ dsw-L11/dsw-L11/src/main/java/pe/edu/idat/dswL11/DswL11Application.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.dswL11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswL11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DswL11Application.class, args);
+	}
+
+}
---
+++ dsw-L11/dsw-L11/src/test/java/pe/edu/idat/dswL11/DswL11ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.dswL11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswL11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
